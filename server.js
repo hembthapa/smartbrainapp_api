5232391c3b17259ee6633812f0fea72ebf226662
HEM B THAPA <hem.b.thapa07@gmail.com>
2020-04-18 19:14:48 +1000

    updating host

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
 			client: 'pg',
 			connection: {
-			host : 'postgresql-graceful-43629',
-			user : 'postgres',
-			password : 'postgres',
-			database : 'smartbrain'
+			host : process.env.DATABASE_URL,
+			ssl: true;
   }
 });
 
