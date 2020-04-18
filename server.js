73d54247af5a3e553ab389b85e02aa3d1f103673
HEM B THAPA <hem.b.thapa07@gmail.com>
2020-04-18 19:11:53 +1000

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
 			client: 'pg',
 			connection: {
-			host : '127.0.0.1',
+			host : 'postgresql-graceful-43629',
 			user : 'postgres',
 			password : 'postgres',
 			database : 'smartbrain'
