b5cdc63d1a322aacdd8e89fe30d96512c38444ea
Hem B. Thapa <hem.b.thapa07@gmail.com>
2020-04-18 23:45:14 +1000

    Add files via upload

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+// initialize with your api key. This will also work in your browser 
+//via http://browserify.org/
+const app = new Clarifai.App({
+ apiKey: 'Enter your own API key'
+}); 
+
+const handlerApiCall = (req, res) => {
+	app.models.predict(Clarifai.FACE_DETECT_MODEL,
+						req.body.input)
+	.then(data => {
+		res.json(data);
+	}).catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handlerImage = (req,res,db) => {
+	const {id} = req.body;
+	db('users').where('id', '=',id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+
+}
+
+module.exports = {
+	handlerImage,
+	handlerApiCall
+}
